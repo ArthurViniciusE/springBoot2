f3e53b99ca419e63c842cdb1009af2faf4e509c4
ArthurBarbosaE <arthur.barbosa@tascominformatica.com.br>
2022-10-13 15:38:21 -0300

    getPart02

---
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -0,0 +1,11 @@
+package academy.devdojo.springboot2.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Anime  {
+    private long id;
+    private String name;
+}
